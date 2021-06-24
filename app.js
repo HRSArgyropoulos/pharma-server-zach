88f725c18b8e00d25b47bb2d7dc40b8382f0555f
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-06-24 14:39:54 +0300

    setup express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+// create express application
+const app = express();
+
+// listen for connections on this host & port
+app.listen(process.env.PORT, process.env.HOST, () => {
+  console.log(
+    `Pharma application listening on http://${process.env.HOST}:${process.env.PORT}`
+  );
+});
