3b89ba2a27375e1b85936312a578110ffb5463d5
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-06-26 23:09:54 +0300

    getTerms fetch + map for UI

--- routes/api/getTerms.js
+++ routes/api/getTerms.js
@@ -3,24 +3,50 @@ const axios = require('axios');
 
 router.get('/', async (req, res) => {
   // get param keys from request
-  const { page, size } = req.query;
+  const { from, size } = req.query;
+  // validation
+  // check if param keys are integers
+  if (
+    !Number.isInteger(+from) ||
+    !Number.isInteger(+size)
+  ) {
+    return res
+      .status(400)
+      .json({ error: 'Invalid Parameters' });
+  }
   // fetch data
   try {
     const response = await axios.get(
       `${process.env.PHARMA_DATA_URL}/terms`,
       {
         params: {
-          page: page,
+          page: from,
           size: size,
         },
+        timeout: 10000,
       }
     );
     const termsData = response.data._embedded.terms;
-    return res.status(200).json(termsData);
+    const responseData = termsData.map((term) => {
+      return {
+        key: term.obo_id,
+        label: term.label,
+        synonyms: term.synonyms
+          ? term.synonyms.join(', ')
+          : '-',
+        obo_id: term.obo_id,
+        term_editor: term.annotation['term editor']
+          ? term.annotation['term editor'].join(', ')
+          : '-',
+        has_children: term.has_children,
+      };
+    });
+    return res.status(200).json({
+      terms: [...responseData],
+      count: size,
+    });
   } catch (err) {
-    return res
-      .status(400)
-      .json({ error: err.message });
+    res.status(400).json({ error: err.message });
   }
 });
 
