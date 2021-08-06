7725c6dcb1af72da114a90f52dc2bfb6278037aa
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-08-06 18:23:10 +0300

    add error logger - morgan

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const { createDbTerms } = require('./database/actions/createDbTerms');
 
+// error logging - Morgan
+const fs = require('fs');
+const path = require('path');
+const morgan = require('morgan');
+// setup morgan tokens
+morgan.token('status', (req, res) => {
+  return res.error.statusCode;
+});
+morgan.token('errorMessage', (req, res) => {
+  return res.error.errorMessage;
+});
+
+// create write stream for logger
+const loggerStream = fs.createWriteStream(
+  path.join(`${__dirname}/logs`, 'error.log'),
+  {
+    flags: 'a',
+  }
+);
+
 // create express application
 const app = express();
 
@@ -27,10 +47,21 @@ app.use((err, req, res, next) => {
   next(err);
 });
 
-// send error response
+// error handling sender
 app.use((err, req, res, next) => {
   res.status(err.statusCode).json({ message: err.errorMessage });
+  next(); // proceed to error logging
 });
+
+// error logging
+app.use(
+  morgan(
+    ':date - :method - :url - status: :status - error_message: :errorMessage - :res[content-length] - :response-time ms',
+    {
+      stream: loggerStream,
+    }
+  )
+);
 
 // listen for connections on this host & port
 app.listen(process.env.PORT, process.env.HOST, () => {
