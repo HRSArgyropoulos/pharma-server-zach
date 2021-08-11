133e1f6bf310851250c00b61cca046a92ceb3fb1
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-08-11 12:59:21 +0300

    updateTerm swagger 11/8

--- validation/schemaValidate.js
+++ validation/schemaValidate.js
@@ -24,4 +24,21 @@ const paramsSchemaValidate = (req, next, schema) => {
   }
 };
 
-module.exports = { bodySchemaValidate, paramsSchemaValidate };
+const termBodySchemaValidate = (req, next, schema) => {
+  const { error, value } = schema.validate(req.body.term);
+  if (error) {
+    next({
+      statusCode: 400,
+      errorMessage: error.details[0].message,
+    });
+  } else {
+    req.body = value;
+    next();
+  }
+};
+
+module.exports = {
+  bodySchemaValidate,
+  paramsSchemaValidate,
+  termBodySchemaValidate,
+};
--- validation/term.js
+++ validation/term.js
@@ -1,19 +1,17 @@
 const Joi = require('Joi');
-const { bodySchemaValidate } = require('./schemaValidate');
+const { termBodySchemaValidate } = require('./schemaValidate');
 
 const updateTermValidation = (req, res, next) => {
   // update term validation schema
   const updateTermSchema = Joi.object({
-    key: Joi.string().required().trim(),
     label: Joi.string().required().trim(),
     synonyms: Joi.string().required().trim(),
-    obo_id: Joi.string().required().trim(),
-    term_editor: Joi.string().required().trim(),
+    term_editor: Joi.string().allow('').trim(),
     has_children: Joi.boolean().required(),
   });
 
   // validate with req body
-  bodySchemaValidate(req, next, updateTermSchema);
+  termBodySchemaValidate(req, next, updateTermSchema);
 };
 
 module.exports = { updateTermValidation };
